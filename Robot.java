4bfd4f825817b386856427078aa34d39689848b9
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2023-02-07 06:04:36 -0500

    Update Robot.java
    We added a new feature so that the left joystick drives half the speed slower than the right joystick.

--- Robot.java
+++ Robot.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
@@ -20,12 +20,12 @@ public class Robot extends TimedRobot {
   private final WPI_VictorSPX m_leftRearMotor = new WPI_VictorSPX(6);
   private final WPI_VictorSPX m_rightRearMotor = new WPI_VictorSPX(2);
 
-  private final Joystick m_stick = new Joystick(0);
+  private final XboxController m_stick = new XboxController(0);
 
   private final MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftFrontMotor, m_leftRearMotor);
   private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightFrontMotor, m_rightRearMotor);
   private DifferentialDrive diffDrive = new DifferentialDrive(leftMotors, rightMotors);
-  
+
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
@@ -40,6 +40,15 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    diffDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+
+    if(Math.abs(m_stick.getLeftY()) > 0.2 || Math.abs(m_stick.getLeftX()) > 0.2)
+    {
+      diffDrive.arcadeDrive(-m_stick.getLeftY() * 0.5, -m_stick.getLeftX() * 0.5);
+    }
+    else
+    {
+      diffDrive.arcadeDrive(-m_stick.getRightY(), -m_stick.getRightX());
+    }
+    
   }
 }
